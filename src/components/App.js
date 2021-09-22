ce3a35b7ed09f7ead1f3c2585d500cdeb4f4a8c4
peterchap <42451267+peterchap@users.noreply.github.com>
2021-09-22 21:42:12 +0100

    220921

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// curly braces for named export
+
+const App = () => {
+    return (
+        <div>
+            App
+        </div>
+    );
+}
+
+
+export default App;
